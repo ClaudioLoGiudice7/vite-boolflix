3a1b5948c6ebdfc4af644f1c6ae7b85bb2dbf579
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-03 17:15:27 +0100

    api attempt #2

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,6 @@ export default {
     AppHeader, AppMain, CardsList, Card
   },
 
-  methods: {
-    getResult() {
-      axios.get("https://api.themoviedb.org/3/search/movie?api_key=c62e8b24ab2d74189b7297f58711d77f")
-        .then((response) => {
-          response.data.response
-        })
-    }
-  }
 };
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,19 @@
 <script>
 export default {
+    data() {
+        return {
 
+        }
+    },
+
+    methods: {
+        getResult() {
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key=c62e8b24ab2d74189b7297f58711d77f")
+                .then((response) => {
+                    response.data.response
+                })
+        }
+    }
 }
 </script>
 
@@ -8,7 +21,7 @@ export default {
     <header class="d-flex align-items-center justify-content-end">
         <nav>
             <input type="search" name="" id="">
-            <button type="button" class="ms-2 me-3">Search</button>
+            <button type="button" class="ms-2 me-3" @click="getResult()">Search</button>
         </nav>
     </header>
 </template>
