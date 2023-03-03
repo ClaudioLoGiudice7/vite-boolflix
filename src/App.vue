3c922d6a1b7067a2ed6b92d79af1a516f766ab78
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-03 15:57:15 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+// IMPORT DEL COMPONENTE
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import CardsList from "./components/CardsList.vue";
+import Card from "./components/Card.vue";
+
+// DEFINIZIONE DEL COMPONENTE IN PAGINA
+export default {
+
+  components: {
+    AppHeader, AppMain, CardsList, Card
+  },
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <CardsList></CardsList>
+  <Card></Card>
+</template>
+
+
+<style lang="scss"></style>
