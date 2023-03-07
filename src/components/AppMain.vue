7eade2c6dabd400b2a0cd9c8b8eb0054860df53f
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-07 18:24:34 +0100

    entering half of milestone 2 (flags) and removing 'v-if="films.length"' and "<h2>         Inizia la ricerca     </h2>" because it appeared "inizia la ricerca" at the end of the list and i didn't know how to solve it

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,27 +13,32 @@ export default {
     props: {
         films: Array,
     },
+
+    methods: {
+        getFlag(country) {
+            country = country.toUpperCase();
+
+            if (country == "EN") return "https://flagsapi.com/GB/flat/64.png"
+            return "https://flagsapi.com/" + country + "/flat/64.png";
+        }
+    }
 };
 
 </script>
 
 <template>
-    <div v-if="films.length">
+    <div>
         <h2>Films</h2>
         <ul v-for="movie in store.movieList" class="mt-4">
             <li>title: {{ movie.title }}</li>
             <li>original_title: {{ movie.original_title }}</li>
             <!-- <li>original_language: {{ movie.original_language }}</li> -->
             <li>
-                <img :src="'https://flagsapi.com/BE/flat/64.png'">
+                <img :src="getFlag(movie.original_language)">
             </li>
             <li>vote_average: {{ movie.vote_average }}</li>
         </ul>
     </div>
-
-    <h2>
-        Inizia la ricerca
-    </h2>
 </template>
 
 
